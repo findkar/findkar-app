48945f4147310f6b50d3da45bd439bbd840371df
Matthew Yeung <csyeung87@gmail.com>
2015-09-16 11:16:40 -0400

    more stuff

--- locations/app/assets/javascripts/googlemap.js
+++ locations/app/assets/javascripts/googlemap.js
@@ -9,7 +9,22 @@ function initialize() {
   var map=new google.maps.Map(document.getElementById("map"),mapProp);
 }
 
+//------ GEOLOCATION
+var x = document.getElementById("demo");
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;	
+    // console.log(position.coords.latitude)
+}
 
 
 
